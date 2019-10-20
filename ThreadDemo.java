47cf57479241e0861e8ed22c382ca2085cb5d2b1
cmking54 <cmking5496@gmail.com>
2019-10-20 16:31:31 -0400

    Refactor

--- ThreadDemo.java
+++ ThreadDemo.java
@@ -1,24 +1,21 @@
 public class ThreadDemo {
   public static void main(String[] args) {
-    Runnable obj1 = new Runnable() {
+
+    Thread t1 = new Thread(new Runnable() {
       public void run() {
         for (int i = 0; i < 5; i++) {
           System.out.println("Hi");
           try { Thread.sleep(500); } catch (Exception e) {}
         }
       }
-    };
-    Runnable obj2 = new Runnable() {
-      public void run() {
-        for (int i = 0; i < 5; i++) {
-          System.out.println("Hello");
-          try { Thread.sleep(500); } catch (Exception e) {}
-        }
-      }
-    };
+    });
 
-    Thread t1 = new Thread(obj1);
-    Thread t2 = new Thread(obj2);
+    Thread t2 = new Thread(() -> {
+      for (int i = 0; i < 5; i++) {
+        System.out.println("Hello");
+        try { Thread.sleep(500); } catch (Exception e) {}
+      }
+    });
 
     t1.start();
     t2.start();
