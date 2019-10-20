ed65b64be244f60397608c7d8ce31897bc07f5b5
cmking54 <cmking5496@gmail.com>
2019-10-20 16:53:58 -0400

    priority

--- ThreadDemo.java
+++ ThreadDemo.java
@@ -8,15 +8,21 @@ public class ThreadDemo {
           try { Thread.sleep(500); } catch (Exception e) {}
         }
       }
-    });
+    }, "Hi Thread");
 
     Thread t2 = new Thread(() -> {
       for (int i = 0; i < 5; i++) {
         System.out.println("Hello");
         try { Thread.sleep(500); } catch (Exception e) {}
       }
-    });
+    }, "Hello Thread");
 
+    System.out.println(t1.getName());
+    System.out.println(t2.getName());
+    t1.setPriority(Thread.MIN_PRIORITY); // 1
+    t2.setPriority(Thread.MAX_PRIORITY); // 10
+    System.out.println(t1.getPriority());
+    System.out.println(t2.getPriority());
     t1.start();
     t2.start();
     System.out.println(t1.isAlive());
