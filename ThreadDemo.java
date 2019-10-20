f778912cfbd86e3d1c01c699782c1fe69fe2179e
cmking54 <cmking5496@gmail.com>
2019-10-20 16:26:09 -0400

    Runnable

--- ThreadDemo.java
+++ ThreadDemo.java
@@ -1,13 +1,16 @@
 public class ThreadDemo {
   public static void main(String[] args) {
-    Hi obj1 = new Hi();
-    Hello obj2 = new Hello();
+    Runnable obj1 = new Hi();
+    Runnable obj2 = new Hello();
 
-    obj1.start();
-    obj2.start();
+    Thread t1 = new Thread(obj1);
+    Thread t2 = new Thread(obj2);
+
+    t1.start();
+    t2.start();
   }
 }
-class Hi extends Thread {
+class Hi implements Runnable {
   public void run() {
     for (int i = 0; i < 5; i++) {
       System.out.println("Hi");
@@ -16,7 +19,7 @@ class Hi extends Thread {
   }
 
 }
-class Hello extends Thread {
+class Hello implements Runnable {
   public void run() {
     for (int i = 0; i < 5; i++) {
       System.out.println("Hello");
